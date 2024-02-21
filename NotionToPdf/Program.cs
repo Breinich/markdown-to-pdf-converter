62d6b849658456f55416d11c3e432769090ce5f8
Breinich <vencel@bajnok.hu>
2024-02-21 16:44:39 +0100

    🆕 feat(Program.cs): add program to convert markdown documents to PDF using Pandoc and Mermaid filter
    The new `Program.cs` file contains code that prompts the user to enter the absolute path of a generated .zip file containing markdown documents. It then extracts the contents of the zip file to a `workdir` directory.
    
    The program checks if there are any markdown documents in the `workdir` directory. If there are no documents, it throws an `ApplicationException` with an appropriate error message.
    
    The program creates two file writers, `outWriter` and `errWriter`, to write the output and error logs respectively.
    
    For each markdown document in the `workdir` directory, the program creates a new `Process` object to execute the `pandoc` command with the appropriate arguments. The `pandoc` command converts the markdown document to a PDF file using the `mermaid-filter` filter.
    
    The program captures the standard output and standard error of the `pandoc` process and writes them to the `out.txt` and `err.txt` files respectively.
    
    After converting all the markdown documents, the program closes the file writers.
    
    Note: There is no newline at the end of the file.

---
+++ NotionToPdf/Program.cs
@@ -0,0 +1,46 @@
+﻿using System.Diagnostics;
+using System.IO.Compression;
+
+Console.WriteLine("Enter the absolute path of the generated .zip file containing the markdown document: ");
+
+var workDir = "workdir";
+var path = Console.ReadLine();
+
+ZipFile.ExtractToDirectory(path ?? throw new ArgumentException("The given path is empty."), 
+    workDir);
+
+var docs = Directory.GetFiles(workDir, "*.md");
+if (docs.Length == 0)
+    throw new ApplicationException("There isn't any document to convert.");
+
+var outWriter = File.AppendText("out.txt");
+var errWriter = File.AppendText("err.txt");
+
+foreach (var doc in docs)
+{
+    outWriter.WriteLine($"{doc}:");
+    
+    var conversion = new Process
+    {
+        StartInfo =
+        {
+            FileName = "pandoc",
+            Arguments = $"\"{doc}\" -o \"{Path.ChangeExtension(doc, "pdf")}\" -F mermaid-filter",
+            RedirectStandardInput = true,
+            RedirectStandardOutput = true,
+            RedirectStandardError = true,
+            UseShellExecute = false,
+            CreateNoWindow = true,
+        }
+    };
+
+    conversion.Start();
+
+    outWriter.WriteLine(conversion.StandardOutput.ReadToEnd());
+    errWriter.WriteLine(conversion.StandardError.ReadToEnd());
+
+    conversion.WaitForExit();
+}
+
+outWriter.Close();
+errWriter.Close();
